ba1bf417e9bf1cb56c3814954cc267682f2ec99c
locust49 <slyazid@student.1337.ma>
2022-12-06 21:30:12 +0100

    feat: add responses and related enums

--- api/users/users_controllers.py
+++ api/users/users_controllers.py
@@ -2,6 +2,8 @@ from . import users_services as us
 from .users_models import Users
 from flask import request, Blueprint
 from api.authentication.auth_middlewares import token_required
+from api.consts.responses import SuccessResponse, ErrorResponse
+from api.consts.error_enum import ErrorEnum
 
 users = Blueprint("users", __name__)
 
@@ -10,8 +12,8 @@ users = Blueprint("users", __name__)
 def get_user_by_uuid(user_uuid):
     user = us.find_one(user_uuid)
     if not user:
-        return {"message": "User not found"}, 404
-    return user, 200
+        return ErrorResponse(ErrorEnum.USR_NOT_FOUND).not_found()
+    return SuccessResponse("User found", user).ok()
 
 
 @users.route("/all", methods=["GET"], defaults={"page": 1, "per_page": 10})
